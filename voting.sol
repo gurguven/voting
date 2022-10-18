f6d3ad244b13619e2ae926da43a4ebeaac9d48a9
gurguven <gurguvengur@gmail.com>
2022-10-18 05:42:28 +0200

    Update voting.sol

--- voting.sol
+++ voting.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.17; 
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
